f5e008171216602c09b79c77ae118cf6ecf18e7e
cstark <student@cstark.local>
2017-01-19 09:58:53 -0600

    second Commit

--- WeatherViewController.swift
+++ WeatherViewController.swift
@@ -17,7 +17,8 @@ class WeatherViewController: UIViewController
     {
         super.viewDidLoad()
         
-        if let url = URL(string: "https://api.forecast.io/forecast/d3250bf407f0579c8355cd39cdd4f9e1/37.7833,122.4167") {
+        if let url = URL(string: "https://api.forecast.io/forecast/d3250bf407f0579c8355cd39cdd4f9e1/37.7833,122.4167")
+        {
             if let data = try? Data(contentsOf: url)
             {
                 do
@@ -41,7 +42,11 @@ class WeatherViewController: UIViewController
     
     func parse(json: JSON)
     {
-        
+        for data in json["data"].arrayValue
+        {
+            
+        }
+
     }
 
 }
