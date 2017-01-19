7130444319570bc7794613136137f6f86ae742ad
Jose Ramon Soto <student@meme-macbook.local>
2017-01-19 09:16:01 -0600

    Files

---
+++ WeatherViewController.swift
@@ -0,0 +1,42 @@
+//
+//  WeatherViewController.swift
+//  AppleTVKit
+//
+//  Created by Jose Ramon Soto on 1/18/17.
+//  Copyright © 2017 Jose Ramon Soto. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController
+{
+    @IBOutlet weak var currentTemperature: UILabel!
+    @IBOutlet weak var summary: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let url = URL(string: "https://api.forecast.io/forecast/d3250bf407f0579c8355cd39cdd4f9e1/37.7833,122.4167") {
+            if let data = try? Data(contentsOf: url){
+                do {
+                    let parsed = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments)
+                    
+                    var newDict = parsed as? NSDictionary
+                    print(newDict!["currently"], ["summary"])
+                    
+                    self.currentTemperature.text = "\(summary)"
+                    self.summary.text = "\(currentTemperature)"
+                    
+                }
+                catch let error as NSError {
+                    print("A JSON parsithng error occurred, here are the details:\n \(error)")
+                }
+            }
+        }
+    }
+    
+    func parse(json: JSON)
+    
+
+}
+//newDict!["currently"], ["temperature"]
